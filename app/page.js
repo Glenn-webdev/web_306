1abae26bfc66f3da884a08343ce5ba82ece14b8d
glenn.luna@edu.sait.ca <glenn.luna@edu.sait.ca>
2024-08-07 17:16:12 -0600

    first commit

---
+++ app/page.js
@@ -0,0 +1,25 @@
+
+
+
+import React from "react";
+import  Nav  from "./components/nav";
+
+export default function Home() {
+  return (
+    <>
+    
+    <Nav/>
+    
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+  
+        <h1>Home</h1>
+     
+
+
+      </div>
+     
+    </main>
+      </>
+  );
+}
---
+++ app/style/styles.js
@@ -0,0 +1,7 @@
+const styles = {
+
+    navBar: {
+        backgroundColor: 'red',
+        color: 'white'
+    }   
+}
