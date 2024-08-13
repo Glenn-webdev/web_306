e9d4b2051a912c839e58cc0714937f45802d4c10
Samuel <141067461+Sgmonsod@users.noreply.github.com>
2024-08-13 16:07:50 -0600

    Update page.js

--- app/pages/contact/page.js
+++ app/pages/contact/page.js
@@ -1,27 +1,45 @@
 import React from 'react';
 import Nav from '../../components/nav';
 import Footer from '../../pages/footer.js';
-import CaregiversList from '../../components/caregiverList.js';
-
 
 export default function Page() {
-
     return (
-
         <>
-        <div className='min-h-screen justify-center'>
-            <Nav></Nav>
-           
-            <h1 className='text-3xl'>Contact us</h1>
-
-            <p>Phone: 123-456-7890</p>
-            <p>Email: 6g8vI@example.com</p> 
-       
-
-       
-        </div>
-        <Footer/>
+            <div className='min-h-screen flex flex-col'>
+                <Nav />
+                <div className="relative w-screen h-screen flex items-center justify-center">
+                    <img
+                        src='/images/doctors.jpg'
+                        className="absolute inset-0 w-full h-full object-cover"
+                        alt="Doctors"
+                    />
+                    <div className="absolute inset-0 bg-black bg-opacity-40"></div>
+                    <div className="relative z-10 p-6 mt-48">
+                        <div className="border-4 border-white rounded-lg p-8 max-w-screen-lg mx-auto bg-transparent">
+                            <div className="mb-8">
+                                <h1 className="text-4xl md:text-5xl font-bold text-center text-white">
+                                    Get in touch with us!
+                                </h1>
+                            </div>
+                            <div className="flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 md:space-x-8">
+                                <div className="flex flex-col items-center  p-4 bg-transparent">
+                                    <h2 className="text-2xl font-semibold text-white">Address</h2>
+                                    <p className="text-white">123 Health Street, Wellness City</p>
+                                </div>
+                                <div className="flex flex-col items-center  p-4 bg-transparent">
+                                    <h2 className="text-2xl font-semibold text-white">Phone</h2>
+                                    <p className="text-white">(123) 456-7890</p>
+                                </div>
+                                <div className="flex flex-col items-center p-4 bg-transparent">
+                                    <h2 className="text-2xl font-semibold text-white">Email</h2>
+                                    <p className="text-white">info@healthcare.com</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <Footer />
+            </div>
         </>
     );
-    
-};
+}
