14d8a5be6f85b4bdbe8421b7a4243b2215145db8
Samuel <141067461+Sgmonsod@users.noreply.github.com>
2024-08-13 16:08:36 -0600

    Update page.js

--- app/pages/register/page.js
+++ app/pages/register/page.js
@@ -1,23 +1,28 @@
-
-
+import React from 'react';
 import RegistrationForm from "../../components/registrationForm";
 import Nav from '../../components/nav';
 import Footer from '../../pages/footer';
+
 export default function Page() {
     return (
         <>
-        <Nav></Nav>
-        <div className="min-h-screen py-11">
-           
-        <RegistrationForm/>
+            <Nav />
+            <div className="relative w-screen min-h-screen flex flex-col">
+                <img
+                    src='/images/patient.jpg'
+                    className="absolute inset-0 w-full h-full object-cover"
+                    alt="Doctors"
+                />
+                <div className="absolute inset-0 bg-green-200 bg-opacity-50"></div>
 
-        </div>
-      
-        <Footer/>
+                <div className="relative z-10 flex-grow flex items-center justify-center">
+                    <div className=" p-8 max-w-screen-lg mx-auto">
+                        <RegistrationForm />
+                    </div>
+                </div>
+
+            </div>
+            <Footer />
         </>
-
-      
-
-        
-    )
-};
+    );
+}
