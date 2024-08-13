daa5a40812d851b5c499407dcc1561140ff01bea
Samuel <141067461+Sgmonsod@users.noreply.github.com>
2024-08-13 16:09:26 -0600

    Update page.js

--- app/pages/page.js
+++ app/pages/page.js
@@ -5,6 +5,7 @@ import React from 'react';
 
 import NavBar from '../components/nav';
 import LoginForm from '../components/loginForm';
+import Footer from './footer';
 
 function LoginPage() {
     return (
@@ -14,7 +15,7 @@ function LoginPage() {
 
           <LoginForm/>
 
-          <h1>login</h1>
+          <Footer/>
 
         
           
